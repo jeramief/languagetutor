a87b72a4c0e28e69edf26ff811c048b5ef3e00ce
Jeramie <forbesjeramie@gmail.com>
2024-08-04 08:56:18 -0400

    added openai and inputs getting received

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,5 +1,16 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 
 def chatbot(request):
+    if request.method == "POST":
+        message = request.POST.get("message")
+        response = "Howdy"
+
+        print("in if")
+
+        return JsonResponse({"message": message, "response": response})
+
+    print("out if")
+
     return render(request, "chatbot.html")
